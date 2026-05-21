828eb4ff49bd62d69d3ff0cd07f2c02876c6e8a8
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-20 23:55:59 -0300

    feat: adiciona o construtor parametrizado

--- src/poo/Pessoa.java
+++ src/poo/Pessoa.java
@@ -4,11 +4,17 @@ public class Pessoa {
     String name;
     int age;
 
-
-
+    //Construtor sem parâmetros
     public Pessoa(){
         this.name = "Sem nome";
         this.age = 0;
+    }
+
+
+    //Construtor parametrizado
+    public Pessoa(String name, int age){
+        this.name = name;
+        this.age = age;
     }
 
     
