c4ac2e72cf292dd3f27fe51fe71ae5177719a066
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-21 15:39:59 -0300

    feat: cria condicional

--- src/basico/controle_fluxo/CaixaEletronico.java
+++ src/basico/controle_fluxo/CaixaEletronico.java
@@ -4,5 +4,9 @@ public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
         double valorSolicitado = 10.5;
+
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+        }
     }
 }
