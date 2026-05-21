4023fa50241e7c413172f1b9a43530a665ced0ea
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-21 19:59:01 -0300

    feat: adiciona enhancer for.

--- src/basico/controle_fluxo/loops/ListarFrutas.java
+++ src/basico/controle_fluxo/loops/ListarFrutas.java
@@ -4,8 +4,14 @@ public class ListarFrutas {
     public static void main(String[] args) {
         String[] frutas = { "Morango", "Uva", "Pera", "Banana", "Kiwi"};
 
+        /* 
         for(int i = 0; i < frutas.length; i++){
             System.out.println(frutas[i]);
+        }
+        */
+
+        for(String fruta : frutas){
+            System.out.println(fruta);
         }
     }
 }
