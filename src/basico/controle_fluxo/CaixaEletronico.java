6954d0512b7ce5646be7d84b3012c3f84adb76b0
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-21 15:39:06 -0300

    feat: adiciona os atributos

--- src/basico/controle_fluxo/CaixaEletronico.java
+++ src/basico/controle_fluxo/CaixaEletronico.java
@@ -2,6 +2,7 @@ package basico.controle_fluxo;
 
 public class CaixaEletronico {
     public static void main(String[] args) {
-        
+        double saldo = 25.0;
+        double valorSolicitado = 10.5;
     }
 }
