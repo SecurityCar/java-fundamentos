35be9166487c15c19a3824aab9187d2b93961bd9
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-21 19:57:01 -0300

    feat: adiciona o arquivo ListarFrutas.java e sua estrutura inicial.

---
+++ src/basico/controle_fluxo/loops/ListarFrutas.java
@@ -0,0 +1,11 @@
+package basico.controle_fluxo.loops;
+
+public class ListarFrutas {
+    public static void main(String[] args) {
+        String[] frutas = { "Morango", "Uva", "Pera", "Banana", "Kiwi"};
+
+        for(int i = 0; i < frutas.length; i++){
+            System.out.println(frutas[i]);
+        }
+    }
+}
