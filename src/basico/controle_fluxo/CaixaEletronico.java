f13076f944aa8b72e0d45834e8cf55c22c2ab029
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-21 15:48:46 -0300

    feat: adiciona caso para valor solicitado for maior que o saldo disponível

--- src/basico/controle_fluxo/CaixaEletronico.java
+++ src/basico/controle_fluxo/CaixaEletronico.java
@@ -5,8 +5,11 @@ public class CaixaEletronico {
         double saldo = 25.0;
         double valorSolicitado = 10.5;
 
-        if(valorSolicitado < saldo){
+        if(valorSolicitado <= saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println("O saldo atual é de: " + saldo);
+        }else{
+            System.out.println("O valor solicitado é maior do que o saldo disponível.");
         }
 
         System.out.println(saldo);
