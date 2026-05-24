63a71d843ead2bbe236f028bb5c54c4988133cc1
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-24 12:31:03 -0300

    feat: cria instanciações da classe Cliente.

--- src/poo/banco/MainBanco.java
+++ src/poo/banco/MainBanco.java
@@ -1,5 +1,7 @@
 package poo.banco;
 
+import java.util.ArrayList;
+
 import poo.banco.cliente.Cliente;
 import poo.banco.conta.Conta;
 import poo.banco.conta.ContaSimples;
@@ -9,4 +11,18 @@ import poo.banco.exceptions.SaldoInsuficienteException;
 
 public class MainBanco {
     
+    public static void main(String[] args) {
+        System.out.println("Banco Digital || Demonstração dos 4 pilares da POO");
+
+        Cliente vitor = new Cliente("Vitor Carvalho", "12345678901", "vitor@email.com");
+        Cliente ana = new Cliente("Ana Silva", "09876543210", "ana@email.com");
+
+        System.out.println("Clientes criados (encapsulamento)");
+        System.out.println(vitor);
+        System.out.println(ana);
+        System.out.println("---------");
+
+
+        
+    }
 }
