19cfe182b51461e726b91487aca46c5426514a79
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-24 11:24:05 -0300

    feat: adiciona método toString da classe Cliente.

--- src/poo/banco/cliente/Cliente.java
+++ src/poo/banco/cliente/Cliente.java
@@ -22,4 +22,9 @@ public class Cliente{
     public String getEmail(){
         return email;
     }
+
+    @Override
+    public String toString(){
+        return "Cliente " + nome + " | CPF: " + cpf;
+    }
 }
