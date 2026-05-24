ae96e38ae50a3a930959403f3800e208908c3217
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-24 12:25:16 -0300

    feat: cria classe MainBanco e importa as classes Cliente, Conta, ContaSimples e SaldoInsuficienteException.

---
+++ src/poo/banco/MainBanco.java
@@ -0,0 +1,12 @@
+package poo.banco;
+
+import poo.banco.cliente.Cliente;
+import poo.banco.conta.Conta;
+import poo.banco.conta.ContaSimples;
+import poo.banco.exceptions.SaldoInsuficienteException;
+
+
+
+public class MainBanco {
+    
+}
