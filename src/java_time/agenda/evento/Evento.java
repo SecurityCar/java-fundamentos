e536f03c05213a7096fb5c7b66e8cfe311976b78
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-25 09:14:20 -0300

    feat: inicializa os atributos de Evento.java

--- src/java_time/agenda/evento/Evento.java
+++ src/java_time/agenda/evento/Evento.java
@@ -1,5 +1,12 @@
 package java_time.agenda.evento;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Evento {
     
+    private String titulo;
+    private String descricao;
+    private String local;
+    private LocalDateTime dataHora;
 }
