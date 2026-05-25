9b9f97d584a7ba61b728c436743b6cd3ee4f8b32
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-25 09:45:07 -0300

    feat: cria o atributo da classe DataUtil.

--- src/java_time/agenda/util/DataUtil.java
+++ src/java_time/agenda/util/DataUtil.java
@@ -1,5 +1,9 @@
 package java_time.agenda.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DataUtil {
     
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 }
