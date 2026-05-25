e703b346ee169e55ed0d91996bdd66f758ecedff
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-25 09:41:00 -0300

    feat: adiciona método toString da classe Evento

--- src/java_time/agenda/evento/Evento.java
+++ src/java_time/agenda/evento/Evento.java
@@ -1,7 +1,9 @@
 package java_time.agenda.evento;
 
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
     
@@ -40,5 +42,17 @@ public class Evento {
     public boolean vaiAcontecerHoje(){
         //Transforma a dataHora em um formato e compara com o LocalDateTime.now(), também transformado
         return dataHora.toLocalDate().equals(LocalDateTime.now().toLocalDate());
+    }
+
+    @Override
+    public String toString(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        String status = estaAtrasado() ? "⚠️ (Está atrasado)" : "✅ (Você ainda pode vir)";
+
+        return "📅 " + titulo + "\n" +
+           "   📍 " + local + "\n" +
+           "   🕒 " + dataHora.format(formatter) + "\n" +
+           "   " + descricao + "\n" +
+           "   " + status;
     }
 }
