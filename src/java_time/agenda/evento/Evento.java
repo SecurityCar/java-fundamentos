4fadce4a6c3d11efac997f8455a90a766b89635c
Vitor Carvalho <vitorclocked@gmail.com>
2026-05-25 09:15:43 -0300

    feat: adiciona construtor da classe Evento

--- src/java_time/agenda/evento/Evento.java
+++ src/java_time/agenda/evento/Evento.java
@@ -9,4 +9,11 @@ public class Evento {
     private String descricao;
     private String local;
     private LocalDateTime dataHora;
+
+    public Evento(String titulo, String descricao, String local, LocalDateTime dataHora){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.local = local;
+        this.dataHora = dataHora;
+    }
 }
